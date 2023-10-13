24a56550a2aaf07bf648d359435dabc88c982426
Mukii <mukii2307@gmail.com>
2023-10-13 14:52:57 +0530

    Update @htaccess.php

--- @htaccess.php
+++ @htaccess.php
@@ -4,3 +4,26 @@
 
 RewriteCond %{HTTPS} !=on
 RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301] 
+
+
+/***
+***** Remove access for all pages
+***/
+deny from all
+
+/***
+***** Remove page extensions from URL
+***/
+PHP Extension:
+
+RewriteEngine on
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %{REQUEST_FILENAME}\.php -f
+RewriteRule ^(.*)$ $1.php
+
+HTML Extension:
+
+RewriteEngine on
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %{REQUEST_FILENAME}\.html -f
+RewriteRule ^(.*)$ $1.html
